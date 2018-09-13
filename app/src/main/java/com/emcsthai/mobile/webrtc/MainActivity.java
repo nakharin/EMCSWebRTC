172f3d30a0cb2729d2e4b92a6e5791c351519c09
nakharin-mac-mini <nakharin.sang@gmail.com>
2018-09-13 17:32:23 +0700

    - Added button style

--- app/src/main/java/com/emcsthai/mobile/webrtc/MainActivity.java
+++ app/src/main/java/com/emcsthai/mobile/webrtc/MainActivity.java
@@ -4,12 +4,14 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +20,11 @@ public class MainActivity extends AppCompatActivity {
     public final static String KEY_ROOM_ID = "KEY_ROOM_ID";
 
     private AppCompatEditText edtRoomId;
+
     private AppCompatButton btnJoin;
+
+    private AppCompatButton btnEnable;
+    private AppCompatButton btnDisable;
 
     private ProgressDialog progressDialog;
 
@@ -30,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         initWidgets();
 
         btnJoin.setOnClickListener(onClickListener);
+        btnEnable.setOnClickListener(onClickListener);
+        btnDisable.setOnClickListener(onClickListener);
         edtRoomId.setOnEditorActionListener(onEditorActionListener);
     }
 
@@ -40,7 +48,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         edtRoomId = findViewById(R.id.edtRoomId);
+
         btnJoin = findViewById(R.id.btnJoin);
+        btnEnable = findViewById(R.id.btnEnable);
+        btnDisable = findViewById(R.id.btnDisable);
 
         progressDialog = new ProgressDialog(MainActivity.this);
         progressDialog.setTitle("");
@@ -66,6 +77,16 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener onClickListener = v -> {
         if (v == btnJoin) {
             checkJoinRoom();
+        }
+
+        if (v == btnEnable) {
+            btnJoin.setEnabled(true);
+            btnJoin.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.btn_active_selector));
+        }
+
+        if (v == btnDisable) {
+            btnJoin.setEnabled(false);
+            btnJoin.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.btn_active_disable));
         }
     };
 
