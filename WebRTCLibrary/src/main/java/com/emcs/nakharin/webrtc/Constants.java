abed1adcfc484aa3031fa046967ea0caec332555
nakharin-mac-mini <nakharin.sang@gmail.com>
2018-12-24 11:51:48 +0700

    - Up version WebRTCLibrary 1.0.1 (2)

--- WebRTCLibrary/src/main/java/com/emcs/nakharin/webrtc/Constants.java
+++ WebRTCLibrary/src/main/java/com/emcs/nakharin/webrtc/Constants.java
@@ -2,7 +2,7 @@ package com.emcs.nakharin.webrtc;
 
 public class Constants {
     public final static String KEY_ROOM_ID = "KEY_ROOM_ID";
-    public final static String BASE_URL = "http://vdosocket.emcsthaidev.com/";
+    public final static String BASE_URL = "https://vdosocket.emcsthaidev.com/";
     public final static String PATH = "/socket/socket.io";
 
     public final static String ICE_SERVER_URL = "turn:203.150.78.92:3478?transport=tcp";
